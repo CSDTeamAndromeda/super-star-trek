0c348b9973c9affab0a1f899defabaadb7cf2b5b
Tim Schoenheider <timrs2998@gmail.com>
2017-03-16 12:11:37 -0500

    fix: Remove proxyquire from testGlobals

--- src/test/testGlobals.js
+++ src/test/testGlobals.js
@@ -3,17 +3,14 @@ const chai = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const chaiAsPromised = require('chai-as-promised')
-const proxyquire = require('proxyquire')
 
 //make globals for easy testing
 global._ = _
 global.should = chai.should()
 global.chai = chai
 global.sinon = sinon
-global.proxyquire = proxyquire
 
 //settings/plugins
 chai.use(sinonChai)
 chai.use(chaiAsPromised)
-proxyquire.noCallThru()
 require('sinon-as-promised')
